d096f3893c90b73939f365e95b7a1acd77523968
Kacper Karpień <kackar212@wp.pl>
2021-04-19 02:13:20 +0200

    add routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-function App() {}
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">Strona główna</Route>
+        <Route path="/profil">Twój profil</Route>
+        <Route path="/dodaj-quiz">Dodaj quiz</Route>
+        <Route>404</Route>
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
