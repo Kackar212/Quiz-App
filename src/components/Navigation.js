83a1aec05e8eb656e6c3e5e62f0b7715bba1b0c9
Kacper Karpień <kackar212@wp.pl>
2021-04-19 05:33:32 +0200

    Add links to navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,19 @@
-export default function Navigation() {}
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Strona główna</Link>
+        </li>
+        <li>
+          <Link to="/profil">Twój profil</Link>
+        </li>
+        <li>
+          <Link to="/dodaj-quiz">Dodaj quiz</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
