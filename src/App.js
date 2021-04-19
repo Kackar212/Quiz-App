36591fb76c6342263e872af845b8aa0c5f0b12ba
Kacper Karpień <kackar212@wp.pl>
2021-04-20 00:48:19 +0200

    Update what is rendered at /

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
-        <Route exact path="/">Strona główna</Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route path="/profil">Twój profil</Route>
         <Route path="/dodaj-quiz">Dodaj quiz</Route>
         <Route>404</Route>
