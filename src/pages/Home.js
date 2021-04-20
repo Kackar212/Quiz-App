615191be3556cba794f33723476cf9ff077839f2
Kacper Karpień <kackar212@wp.pl>
2021-04-20 04:10:46 +0200

    Update what is rendered by Home component
    Home will render list of QuizPreview components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import { useEffect } from "react"
 import { useQuery } from "react-query"
 import { fetchQuizzes } from "../api";
+import QuizPreview from "../components/QuizPreview";
 
 export default function Home() {
   const { data: quizzes, isLoading, isError } = useQuery('quizzes', fetchQuizzes);
 
-  return <div>Strona główna</div>
+  return (
+    <div>
+      { 
+        quizzes.map (
+          ({ id, user, name }) => <QuizPreview id={id} user={user} name={name}/>
+        )
+      }
+    </div>
+  )
 }
