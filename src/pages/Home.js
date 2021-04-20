7fc4466da91ffce47e684abe0deb711becfd3fdf
Kacper Karpień <kackar212@wp.pl>
2021-04-20 03:56:04 +0200

    Add fetching quizzes in Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,9 @@
+import { useEffect } from "react"
+import { useQuery } from "react-query"
+import { fetchQuizzes } from "../api";
+
 export default function Home() {
+  const { data: quizzes, isLoading, isError } = useQuery('quizzes', fetchQuizzes);
+
   return <div>Strona główna</div>
 }
