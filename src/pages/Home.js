d7936cce7a0b984a29e474c43a1686a123d50c2e
unknown <kackar212@gmail.com>
2021-04-22 03:49:35 +0200

    Add Loader to Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
-import { useQuery } from "react-query"
+import { useQuery, useQueryClient } from "react-query"
 import { fetchQuizzes } from "../api";
+import Loader from "../components/Loader";
 import QuizPreview from "../components/QuizPreview";
 
 export default function Home() {
-  const { data: quizzes = [] } = useQuery('quizzes', fetchQuizzes);
+  const { data: quizzes = [], isFetching, isLoading } = useQuery('quizzes', fetchQuizzes);
+
+  if (isLoading) return <Loader />
   
   return (
     <div>
