5894802da55cc505b665043ffbbf660e4906587f
unknown <kackar212@gmail.com>
2021-04-23 19:42:23 +0200

    Add JSX structure to Questions component

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,1 +1,6 @@
-export default function Questions({ questions, answers }) {}
+import { generateKey } from "../../helpers"
+import Question from "../Question"
+
+export default function Questions({ questions }) {
+    return questions.map(({ question, answers }, index) => <Question key={generateKey()} index={index} question={question} questionAnswers={answers}/>)
+}
