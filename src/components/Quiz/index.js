262ee45ff5240428e4d38540975ee90186214b90
unknown <kackar212@gmail.com>
2021-04-23 22:23:57 +0200

    Add JSX structure to Quiz component

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,1 +1,17 @@
-export default function Quiz() {}
+import { useQuery } from "react-query";
+import { useParams } from "react-router";
+import Questions from "../Questions";
+import Tags from "../Tags";
+
+export default function Quiz() {
+    return (
+        <div>
+           <p>{name}</p>
+            <Tags tags={tags}/>           
+            <form data-id={id}>
+                <Questions questions={questions} />
+                <button type="submit">Sprawdź odpowiedzi</button>
+            </form>
+        </div>
+    )
+}
