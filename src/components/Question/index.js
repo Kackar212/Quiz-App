53193ab4081c4f117805e96b6bf19dca1c28cec7
unknown <kackar212@gmail.com>
2021-04-23 20:07:26 +0200

    Add JSX structure to Question component

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,1 +1,10 @@
-export default function Question({ question, questionAnswers }) {}
+import Answers from "../Answers";
+
+export default function Question({ question, questionAnswers, id, index }) {
+    return (
+        <div>
+            <p>{index + 1}. {question}</p>
+            <Answers answers={questionAnswers} questionId={id}/>
+        </div>
+    );
+}
