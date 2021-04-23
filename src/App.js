deef676b6dc137e301527f5f3e8158811e62a8f2
unknown <kackar212@gmail.com>
2021-04-23 23:32:42 +0200

    Add Route for single quiz

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
+import Quiz from "./components/Quiz";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
@@ -9,6 +10,9 @@ function App() {
     <Router>
       <Navigation />
       <Switch>
+        <Route path="/quiz/:quizId/:quizName">
+          <Quiz />
+        </Route>
         <Route exact path="/">
           <Home />
         </Route>
