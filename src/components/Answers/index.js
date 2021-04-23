70bc00977233c260883fc292a4a7915035afbed2
unknown <kackar212@gmail.com>
2021-04-23 20:23:49 +0200

    Add JSX structure to Answers component

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -1,1 +1,6 @@
-export default function Answers({ answers }) {}
+import { generateKey } from "../../helpers";
+import Answer from "../Answer";
+
+export default function Answers({ answers, questionId }) {
+    return answers.map((answer, index) => <Answer key={generateKey()} index={index} answer={answer} questionId={questionId}/> );
+}
