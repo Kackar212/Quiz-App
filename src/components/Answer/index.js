0b51d62d9bc278f4aa451320f2dc255d62d70004
unknown <kackar212@gmail.com>
2021-04-23 21:59:49 +0200

    Add JSX structure to Answer component

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -1,1 +1,12 @@
-export default function Answer({ answer }) {}
+import FormField from "../FormField";
+import Input from "../Input";
+import Label from "../Label";
+
+export default function Answer({ questionId, answer: { answer: content, id }, index }) {
+    return (
+        <FormField>
+            <Input type="checkbox" name={`answer-${questionId}`} id={`answer_${id}`} data-answer-id={id}/>
+            <Label htmlFor={`answer_${id}`}>{index + 1}. {content}</Label>
+        </FormField>
+    );
+}
