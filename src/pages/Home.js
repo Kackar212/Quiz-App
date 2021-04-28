d85c009dcd993b464d351799a6cadcecddb255c8
unknown <kackar212@gmail.com>
2021-04-28 21:21:17 +0200

    Update Home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from "react-query"
+import { useQuery } from "react-query"
 import { fetchQuizzes } from "../api";
 import Loader from "../components/Loader";
 import QuizPreview from "../components/QuizPreview";
