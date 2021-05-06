ad5b9bcd74298346501ecf44150ecae1c39981bb
KacKar212 <kackar212@gmail.com>
2021-05-06 20:06:04 +0200

    Create helpers.js file

---
+++ src/helpers.js
@@ -0,0 +1,28 @@
+export function getKeys(path) {
+    return path.replace(/\[(\d*)\]/g, ".$1").split(".");
+}
+
+export function put(path = "", obj, value) {
+    const keys = getKeys(path);
+    let currObj = obj || {};
+    keys.forEach((key, index) => {
+      if (index === keys.length - 1) {
+        currObj[key] = value;
+      }
+
+      currObj = currObj[key];
+    });
+
+    return obj;
+} 
+
+export function getFromPath(path, obj, defaultValue = "") {
+
+    const keys = getKeys(path);
+
+    return keys.reduce((prev, curr) => {
+      if (typeof prev[curr] === "undefined") return defaultValue;
+      return (prev = prev[curr]);
+    }, obj || {});
+}
+
