b5345421f769833df1566f5dcf8cae57070e9f7a
KacKar212 <kackar212@gmail.com>
2021-05-06 20:02:44 +0200

    Create EasyForm component for handling form values and validation

---
+++ src/components/EasyForm/index.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { getFromPath, put } from "../../helpers";
+import { useValidate } from "../../Validator";
+
+export default function EasyForm({
+    render = () => {},
+    initialValues = {},
+    schema: { schema, validator } = {},
+    onSubmit = () => {},
+}) {
+    const onChangeListener = ({ target: { type, value, checked, name } }) => {
+      const inputValue = type === "radio" || type === "checkbox" ? checked : value;
+      
+      put(name, values, inputValue);
+      setValues({ ...values });
+    };
+
+    const [values, setValues] = useState(initialValues);
+    const { isValid, errors } = useValidate(values, schema, validator);
+
+    function onSubmitListener(ev) {
+      onSubmit({ ev, errors, isValid  })
+    }
+
+    function input(name, inputType) {
+      const value = getFromPath(name, values);
+      const checked = getFromPath(name, values, false);
+      
+      const props = {
+        name,
+        onChange: onChangeListener,
+        value,
+        checked,
+        id: name,
+        type: inputType
+      };
+
+      return props;
+    }
+
+    return render({ setValues, values, input, onSubmit: onSubmitListener, errors, isValid });
+}
