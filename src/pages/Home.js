a1de124b1fa34e34136d12efcbaee2ec1b85468c
KacKar212 <kackar212@gmail.com>
2021-05-08 02:34:04 +0200

    Update Home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,28 @@
 import { useQuery } from "react-query"
 import { fetchQuizzes } from "../api";
 import Loader from "../components/Loader";
+import Message from "../components/Message";
 import QuizPreview from "../components/QuizPreview";
+import { isEmpty } from "../helpers";
 
 export default function Home() {
-  const { data: quizzes = [], isFetching, isLoading } = useQuery('quizzes', fetchQuizzes);
+  const { data: quizzes = [], isLoading } = useQuery('quizzes', fetchQuizzes);
 
   if (isLoading) return <Loader />
-  
+
+  const renderQuizzes = () => {
+    if (isEmpty(quizzes)) {
+      return <Message to="/dodaj-quiz" link="Dodaj quiz" message="Nie ma jeszcze żadnych quizów :("/>
+    }
+
+    return quizzes.map(({ id, user, name }) => (
+      <QuizPreview id={id} user={user} name={name} key={id}/>
+    ))
+  }
+
   return (
     <div>
-      { 
-        quizzes.map (
-          ({ id, user, name }) => <QuizPreview id={id} user={user} name={name}/>
-        )
-      }
+      { renderQuizzes() }
     </div>
   )
 }
