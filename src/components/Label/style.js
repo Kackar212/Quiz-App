3c89e8a5eee0b6089f826f8b078cb6ad2d841ac9
KacKar212 <kackar212@gmail.com>
2021-05-10 22:23:20 +0200

    Update styles for Label

--- src/components/Label/style.js
+++ src/components/Label/style.js
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const StyledLabel = styled.label`
     position: ${({ position = "absolute" }) => position};
     top: 50%;
-    transform: translateY(-50%);
-    left: 15px;
+    transform: ${({ position }) => position !== "static" && "translateY(-50%)"};
+    left: 8px;
     color: ${({ color = "#a09797" }) => color};
     display: flex;
 `;
