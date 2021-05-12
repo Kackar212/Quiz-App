2b47087bedf068591e301c77fda66ddc35691e56
KacKar212 <kackar212@gmail.com>
2021-05-12 09:52:51 +0200

    Update styles for Input

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,29 +1,57 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const styleForCheckbox = css`
+    box-shadow: none;
+    position: absolute;
+    left: -9999px;
+
+    & + label {
+        color: #222;
+        display: flex;
+        align-items: center;
+        text-transform: uppercase;
+        font-size: .725em;
+        padding-left: 8px;
+        position: static;
+    }
+
+    & + label span::after {
+        transition: transform .3s;
+    }
+
+    &:checked + label span::after {
+        transform: translateY(calc(-100% - 2px));
+    }
+`
+
+const labelOnFocus = css`
+    transform: translateY(-100%);
+    top: 1px;
+    transition: transform .3s;
+`
 
 export const StyledInput = styled.input`
-    border: 0;
-    box-shadow: ${({ border }) => border || "0 0 0 1px rgb(0 0 0 / 20%)"};
+    border: ${({ border }) => border || "1px solid rgb(0 0 0 / 20%)"};
     padding: 10px 8px;
     width: 100%;
+    line-height: 1.8;
     border-radius: 0;
-    ${({ type }) => (
-        (type === "checkbox" || type === "radio") && `
-                box-shadow: none;
-                position: absolute;
-                left: -9999px;
-
-                & + label {
-                    color: #222;
-                    display: flex;
-                    align-items: center;
-                    text-transform: uppercase;
-                    font-size: .725em;
-                    padding-left: 8px;
-                }
-
-                &:checked + label span::after {
-                    display: block;
-                }
-        `)
+    
+    & + label {
+        transition: transform .3s;
     }
+
+    &:not([type="checkbox"]):focus + label {
+        ${ labelOnFocus }
+    }
+    
+    &:not([type="checkbox"]):not(:focus) + label {
+        ${({ isNotEmpty }) => isNotEmpty && labelOnFocus}
+    }
+
+    &[type="checkbox"]:focus + label span {
+        box-shadow: 0 0 0 2px;
+    }
+    
+    ${({ type }) => (type === "checkbox" || type === "radio") && styleForCheckbox}
 `;
