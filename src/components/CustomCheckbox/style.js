852017dfb4107083977447b3a4939c1995e3230f
KacKar212 <kackar212@gmail.com>
2021-05-12 09:56:56 +0200

    Update styles for CustomCheckbox

--- src/components/CustomCheckbox/style.js
+++ src/components/CustomCheckbox/style.js
@@ -11,13 +11,15 @@ export const StyledCustomCheckbox = styled.span`
     align-items: center;
     justify-content: center;
     margin-left: 10px;
+    overflow: hidden;
 
     &::after {
         content: '';
         background: url(${checkMark});
-        display: none;
+        display: block;
         background-size: contain;
-        background-position: center;
+        position: absolute;
+        bottom: -18px;
         width: 18px;
         height: 18px;
     }
