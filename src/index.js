8a636bcf880508b33d7da236e4d40c69d5d1978c
KacKar212 <kackar212@gmail.com>
2021-05-30 21:45:57 +0200

    Set staleTime option for QueryClient instance

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { StyleSheetManager } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 import App from './App';
+import { GlobalStyle } from './styles';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({ staleTime: 1000 * 60 });
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
+    <GlobalStyle />
     <QueryClientProvider client={queryClient}>
-      <App />
+      <StyleSheetManager disableVendorPrefixes={true}>
+        <App />
+      </StyleSheetManager>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
