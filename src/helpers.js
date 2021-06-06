0c46a791153e305adf42d9d0f37fe4fbaeb9231d
KacKar212 <kackar212@gmail.com>
2021-06-06 23:34:13 +0200

    Add helpers for cookies

--- src/helpers.js
+++ src/helpers.js
@@ -2,7 +2,8 @@ export function getKeys(path) {
     return path.replace(/\[(\d*)\]/g, ".$1").split(".");
 }
 
-export function put(path = "", obj, value) {
+export function put(path = "", obj, value, newObj = false) {
+    
     const keys = getKeys(path);
     let currObj = obj || {};
     keys.forEach((key, index) => {
@@ -32,4 +33,30 @@ export function hasErrors(...arrays) {
 
 export function isEmpty(arr = []) {
   return !arr.length;
+}
+
+export function createCookie(cookieName, value, options) {
+  const { onExpire, ...rest } = options;
+
+  setTimeout(onExpire, (rest['max-age'] + 10) * 1000);
+  
+  const parsedOptions = Object.entries(rest).map(([k, v]) => `${k}=${v};`).join(' ');
+  document.cookie = `${cookieName}=${value}; ${parsedOptions}`;
+}
+
+export function getCookie(cookieName) {
+  const reg = new RegExp(`(?<=${cookieName}=)(.*)(?=;|)`, 'g');
+
+  const [cookie] = document.cookie.match(reg) || [];
+
+  if (cookie) {
+    return JSON.parse(cookie);
+  }
+}
+
+export function removeCookie(cookieName) {
+  const cookie = getCookie(cookieName);
+  document.cookie = `${cookieName}=${JSON.stringify(cookie)}; max-age=-1;`;
+
+  return !!getCookie(cookieName);
 }
