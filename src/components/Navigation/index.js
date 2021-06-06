58bbdebc37bc6b34c92665b4ef3f3745d725f542
KacKar212 <kackar212@gmail.com>
2021-06-07 00:39:49 +0200

    Add logout link to Navigation component

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,16 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/User";
 import { NavigationLink, NavigationList } from "./styles";
 
+const NavigationLogout = (
+  <li>
+    <NavigationLink to="/wyloguj">Wyloguj</NavigationLink>
+  </li>
+)
+
 export default function Navigation() {
+  const { auth } = useContext(UserContext);
+
   return (
     <nav>
       <NavigationList>
@@ -13,6 +23,7 @@ export default function Navigation() {
         <li>
           <NavigationLink to="/dodaj-quiz">Dodaj quiz</NavigationLink>
         </li>
+        { auth && NavigationLogout }
       </NavigationList>
     </nav>
   )
