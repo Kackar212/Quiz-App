a570f5efb367341ab16d471974ec5d5d7a9a9681
KacKar212 <kackar212@gmail.com>
2021-06-06 23:49:47 +0200

    Add helpers to api.js for auth requests

--- src/api.js
+++ src/api.js
@@ -1,28 +1,62 @@
 const baseURL = 'http://localhost:8080/api';
 
-async function post(url, options) {
-  const { body } = options;
+const defaultOptions = {
+  credentials: 'include',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
 
-  options = {
-    ...options,
+function postOptions(body) {
+  return {
     body: typeof body === 'string' ? body : JSON.stringify(body),
-    headers: {
-      'Content-Type': 'application/json',
-    },
     method: 'post',
+  }
+}
+
+async function post(url, body) {
+  const options = {
+    ...defaultOptions,
+    ...postOptions(body)
   }
 
   return request(`${baseURL}/${url}`, options);
 }
 
-async function get(url, options) {
-  return request(`${baseURL}/${url}`, options);
+async function get(url) {
+  return request(`${baseURL}/${url}`);
 }
 
 async function request(url, options) {
+  console.log(options);
   const response = await fetch(url, options);
 
   return response.json();
+}
+
+async function authRequest(url, method = get, body) {
+  const response = await method(url, body);
+
+  if (response.statusCode === 401) {
+    const response = await refreshToken();
+
+    if (response.statusCode === 401) {
+      
+      return response;
+    }
+
+    return await method(url, body);
+  }
+
+  return response;
+}
+
+export async function authPost(url, body) {
+  return authRequest(url, post, body);
+}
+
+export async function authGet(url) {
+  return authRequest(url);
 }
 
 export async function fetchQuizzes() {
@@ -36,9 +70,13 @@ export async function fetchQuiz({ queryKey }) {
 }
 
 export async function createQuiz(quiz) {
-  return post('quizzes', { body: quiz });
+  return authPost('quizzes', quiz);
 }
 
 export async function createUser(user) {
-  return post('users', { body: user });
+  return post('users', user);
+}
+
+export async function authUser(user) {
+  return post('users/auth', user);
 }
