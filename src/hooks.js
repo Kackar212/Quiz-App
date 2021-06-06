0eb7d6b0716866be1c5c3998ddb50407ff24ba52
KacKar212 <kackar212@gmail.com>
2021-06-06 23:36:44 +0200

    Add hook for user authorization

---
+++ src/hooks.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { isAuth } from "./helpers";
+
+export const useAuth = () => {
+    const [user, setUser] = useState({});
+    const [auth, setAuth] = useState(false);
+    
+    const userAuth = async () => {
+        if (auth !== await isAuth()) {
+            setAuth(!auth);
+        }
+    }
+
+    useEffect(() => {
+      userAuth();
+      setInterval(() => {
+        userAuth();
+      }, 2000);
+    }, []);
+  
+    return { user, setUser, auth, setAuth };
+}
