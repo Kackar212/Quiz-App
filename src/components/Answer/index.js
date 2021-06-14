414b4594d68a558725f229a1c73ba32cdf624401
KacKar212 <kackar212@gmail.com>
2021-06-14 11:48:01 +0200

    Update Answer component

--- src/components/Answer/index.js
+++ src/components/Answer/index.js
@@ -1,12 +1,22 @@
+import CustomCheckbox from "../CustomCheckbox";
 import FormField from "../FormField";
 import Input from "../Input";
 import Label from "../Label";
 
-export default function Answer({ questionId, answer: { answer: content, id }, index }) {
+export default function Answer({ questionId, formState: { errors, input }, result, correctAnswers, answer: { answer: content, id }, index }) {
+    const isCorrectAnswer = result === undefined ? result : !!correctAnswers.find(answer => answer === id);
     return (
-        <FormField>
-            <Input type="checkbox" name={`answer-${questionId}`} id={`answer_${id}`} data-answer-id={id}/>
-            <Label htmlFor={`answer_${id}`}>{index + 1}. {content}</Label>
+        <FormField errors={errors[questionId][index]}>
+            <Input type="checkbox" { ...input(`answers[${questionId}][${index}]`) } disabled={result !== undefined} id={`answer_${questionId}_${index}`} data-answer-id={id}/>
+            <Label 
+                htmlFor={`answer_${questionId}_${index}`}
+                position="static"
+                isCorrectAnswer={isCorrectAnswer}
+                color={result !== undefined && '#fff'}
+            >
+                {index + 1}. {content}
+                <CustomCheckbox />
+            </Label>
         </FormField>
     );
 }
