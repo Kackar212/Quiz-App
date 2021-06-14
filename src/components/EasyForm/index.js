00d8a66c5075c0983381c59c024d8e7d81a68a42
KacKar212 <kackar212@gmail.com>
2021-06-14 11:48:40 +0200

    Update EasyForm component

--- src/components/EasyForm/index.js
+++ src/components/EasyForm/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getFromPath, put } from "../../helpers";
+import { createFromPath, getFromPath, put } from "../../helpers";
 import { useValidate } from "../../Validator";
 
 export default function EasyForm({
@@ -10,24 +10,31 @@ export default function EasyForm({
     mapValues = (values) => values,
 }) {
     const [values, setValues] = useState(initialValues);
-    const { isValid, errors } = useValidate(values, schema, validator);
-
+    const { isValid, errors } = useValidate(values, initialValues, schema, validator);
+    
     async function onSubmitListener(ev) {
       ev.preventDefault();
-      onSubmit({ ev, errors, isValid, values: mapValues(values), setValues });
+      onSubmit({ ev, errors, isValid, originalValues: values, values: mapValues(values), setValues });
     }
 
     function input(name, onChange = () => {}) {
-      const value = getFromPath(name, values);
+      const value = getFromPath(name, values, '');
       const checked = getFromPath(name, values, false);
+
       const onChangeListener = (e) => {
         const { target: { type, value, checked, name } } = e;
         const inputValue = type === "radio" || type === "checkbox" ? checked : value;
-        
-        put(name, values, inputValue);
-        onChange(mapValues(values), e);
-        setValues({ ...values });
+
+        setValues(prevValues => {
+          const newValues = { ...prevValues };
+
+          put(name, newValues, inputValue);
+          onChange(mapValues(newValues), e);
+
+          return newValues;
+        });
       };
+
       const props = {
         name,
         onChange: (e) => {
