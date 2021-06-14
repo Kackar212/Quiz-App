1871cdca8b5abe585a2d17c782afb49b5975282f
KacKar212 <kackar212@gmail.com>
2021-06-14 11:48:06 +0200

    Update Answers component

--- src/components/Answers/index.js
+++ src/components/Answers/index.js
@@ -1,5 +1,16 @@
 import Answer from "../Answer";
 
-export default function Answers({ answers, questionId }) {
-    return answers.map((answer, index) => <Answer key={answer + index} index={index} answer={answer} questionId={questionId}/> );
+export default function Answers({ answers, questionId, formState, checkAnswersResult = [undefined, []] }) {
+    const [result, correctAnswers] = checkAnswersResult;
+    return answers.map((answer, index) => (
+        <Answer
+            key={questionId + '-' + index}
+            correctAnswers={correctAnswers}
+            formState={formState}
+            result={result}
+            index={index}
+            answer={answer}
+            questionId={questionId}
+        /> 
+    ));
 }
