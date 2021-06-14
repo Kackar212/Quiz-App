538d743032c8c74587963f5c798bbd0eba7838c1
KacKar212 <kackar212@gmail.com>
2021-06-14 11:00:05 +0200

    Add render when user is logged in

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,66 @@
+import { useContext } from "react";
+import { useMutation, useQuery } from "react-query";
+import { useHistory } from "react-router";
+import { authGet, authUser, createUser, deleteQuiz } from "../api";
+import AuthForm from "../components/AuthForm";
+import QuizPreview from "../components/QuizPreview";
+import { UserContext } from "../context/User";
+import Loader from "../components/Loader";
+import { isEmpty } from "../helpers";
+import Message from "../components/Message";
+
 export default function Profile() {
-  return (
-    <div>Profil</div>
-  )
+  const { setUser, auth, setAuth } = useContext(UserContext);
+  const { data = [], isLoading, refetch } = useQuery('getUserQuizzes', async () => {
+    return authGet('users/quizzes');
+  });
+  const { mutateAsync, isLoading: removeQuizIsLoading, data: deletedQuizData = {} } = useMutation('removeQuiz', deleteQuiz);
+  const history = useHistory();
+
+  if (!auth) {
+    return (
+      <>
+        <AuthForm
+          title="Zarejestruj się"
+          successMessage='Pomyślnie stworzyłeś konto!'
+          apiFunction={createUser}
+        />
+        <AuthForm 
+          title="Zaloguj się"
+          successMessage='Pomyślnie się zalogowałeś!'
+          apiFunction={authUser}
+          afterRequest={({ name, id } = {}) => {
+            setUser({ name, id });
+            setAuth(true);
+            localStorage.setItem('auth', true);
+            history.goBack();
+          }}
+        />
+      </>
+    );
+  }
+
+  let quizzes = Array.isArray(data) ? data : [];
+  quizzes = quizzes.filter(({ id }) => deletedQuizData.id !== id);
+
+  const renderQuizPreview = ({ id, user, name, categories, questions }) => (
+    <QuizPreview
+      id={id}
+      tags={categories}
+      user={user}
+      name={name}
+      key={id}
+      numOfQuestions={questions.length}
+      removeQuiz={mutateAsync}
+      removedId={deletedQuizData.id}
+      isLoading={removeQuizIsLoading}
+      getUserQuizzes={refetch}
+    />
+  );
+
+  if (isEmpty(quizzes)) {
+    return <Message to="/dodaj-quiz" link="Dodaj quiz" message="Nie masz jeszcze żadnych quizów :("/>
+  }
+
+  return !isLoading ? quizzes.map(renderQuizPreview) : <Loader />;
 }
